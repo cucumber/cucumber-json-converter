adf8534f4a35cffe25fd5bee8a475fd6a70382ef
Gaspar Nagy <gaspar.nagy@gmail.com>
2018-02-22 19:44:04 +0100

    save result

--- src/Cucumber.Pro.SpecFlowPlugin.TestDataGenerator/Program.cs
+++ src/Cucumber.Pro.SpecFlowPlugin.TestDataGenerator/Program.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Cucumber.Pro.SpecFlowPlugin.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using TechTalk.SpecFlow.Tracing;
 
 namespace Cucumber.Pro.SpecFlowPlugin.TestDataGenerator
@@ -19,10 +22,15 @@ namespace Cucumber.Pro.SpecFlowPlugin.TestDataGenerator
             Environment.SetEnvironmentVariable("GIT_BRANCH", "test-master", EnvironmentVariableTarget.Process);
             Environment.SetEnvironmentVariable("CUCUMBERPRO_TESTING_DRYRUN", "true", EnvironmentVariableTarget.Process);
             Environment.SetEnvironmentVariable("CUCUMBERPRO_RESULTS_FILE", "results.json", EnvironmentVariableTarget.Process);
-            ProcessFeature("one-passing-scenario.feature");
+
+            var assemblyFolder = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath) ??
+                                 Directory.GetCurrentDirectory(); // in the very rare case the assembly folder cannot be detected, we use current directory
+            var featuresFolder = Path.GetFullPath(Path.Combine(assemblyFolder, "..", "..", "..", "..", "features"));
+
+            ProcessFeature("one-passing-scenario.feature", assemblyFolder, featuresFolder);
         }
 
-        private static void ProcessFeature(string featureFileName)
+        private static void ProcessFeature(string featureFileName, string assemblyFolder, string featuresFolder)
         {
             string baseName = Path.GetFileNameWithoutExtension(featureFileName);
             string className = baseName.ToIdentifier();
@@ -32,6 +40,12 @@ namespace Cucumber.Pro.SpecFlowPlugin.TestDataGenerator
                 $"--test=Cucumber.Pro.SpecFlowPlugin.TestDataGenerator.Features.{className}Feature",
                 "--inprocess"
             });
+
+            var jsonContent = File.ReadAllText(Path.Combine(assemblyFolder, "results.json"));
+            var obj = JObject.Parse(jsonContent);
+            var token = (JObject)obj.SelectToken("cucumberJson").First();
+            var result = JsonConvert.SerializeObject(token, Formatting.Indented);
+            File.WriteAllText(Path.Combine(featuresFolder, featureFileName + ".specflow.json"), result);
         }
     }
 }
