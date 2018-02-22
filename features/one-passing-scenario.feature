3955197a0fb826817fc729aa0c42f2a8bd76a2fd
Gaspar Nagy <gaspar.nagy@gmail.com>
2018-02-22 17:04:21 +0100

    run a single scenario

--- features/one-passing-scenario.feature
+++ features/one-passing-scenario.feature
@@ -1,3 +1,3 @@
-Feature:
-  Scenario:
+Feature: one-passing-scenario
+  Scenario: Scenario 1
     Given this step passes
--- src/Cucumber.Pro.SpecFlowPlugin.TestDataGenerator/Program.cs
+++ src/Cucumber.Pro.SpecFlowPlugin.TestDataGenerator/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow.Tracing;
 
 namespace Cucumber.Pro.SpecFlowPlugin.TestDataGenerator
 {
@@ -10,6 +13,25 @@ namespace Cucumber.Pro.SpecFlowPlugin.TestDataGenerator
     {
         static void Main(string[] args)
         {
+            Environment.SetEnvironmentVariable("CUCUMBERPRO_LOGGING", "DEBUG", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("CUCUMBERPRO_PROJECTNAME", "test-prj", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("GIT_COMMIT", "test-sha", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("GIT_BRANCH", "test-master", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("CUCUMBERPRO_TESTING_DRYRUN", "true", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("CUCUMBERPRO_RESULTS_FILE", "results.json", EnvironmentVariableTarget.Process);
+            ProcessFeature("one-passing-scenario.feature");
+        }
+
+        private static void ProcessFeature(string featureFileName)
+        {
+            string baseName = Path.GetFileNameWithoutExtension(featureFileName);
+            string className = baseName.ToIdentifier();
+            NUnit.ConsoleRunner.Program.Main(new[]
+            {
+                Assembly.GetExecutingAssembly().Location,
+                $"--test=Cucumber.Pro.SpecFlowPlugin.TestDataGenerator.Features.{className}Feature",
+                "--inprocess"
+            });
         }
     }
 }
