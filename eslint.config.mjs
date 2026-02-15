048bc249817844d4ad58f3a6f5c16939aa97ebb8
David Goss <david@davidgoss.co>
2026-02-15 10:31:53 +0000

    fully migrate config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,31 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
-import _import from 'eslint-plugin-import'
+import importPlugin from 'eslint-plugin-import'
 import n from 'eslint-plugin-n'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
-
 export default [
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:import/typescript',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended'
-    )
-  ),
+  js.configs.recommended,
+  ...typescriptEslint.configs['flat/recommended'],
   {
-    plugins: {
-      import: fixupPluginRules(_import),
-      'simple-import-sort': simpleImportSort,
-      n,
-      '@typescript-eslint': fixupPluginRules(typescriptEslint),
-    },
+    files: ['**/*.ts'],
 
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'module',
-
       parserOptions: {
-        project: 'tsconfig.json',
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
       },
+    },
+
+    plugins: {
+      import: importPlugin,
+      n,
+      'simple-import-sort': simpleImportSort,
     },
 
     rules: {
